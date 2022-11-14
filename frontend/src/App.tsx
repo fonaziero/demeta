e3a90217b72e7a36dca29f0dfb1500bfc0529cef
fonaziero <victorcarnevale@yahoo.com.br>
2022-11-14 18:46:05 -0300

    Fisrt component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
-
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
-  )
+  return (
+    <>
+      <h1>Ola</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
